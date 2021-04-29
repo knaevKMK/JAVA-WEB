8da58a2610f845d900a7b2d24439fbc98050b95f
Kr_Knev <kneav@students.softuni.bg>
2021-04-29 09:14:48 +0300

    reg

---
+++ 02 Spring _User registration/demo_reg_user/src/main/java/registration/DemoRegUserApplication.java
@@ -0,0 +1,13 @@
+package registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRegUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRegUserApplication.class, args);
+	}
+
+}
