cb119e68bd7cd0b9b0a5fa80897cdaf6b17ef1f6
knaevKMK <kneav@students.softuni.bg>
2021-10-13 11:11:08 +0300

    init

--- Spring Fundamentals - jan 2021/EXAM PREPARATION/exam2/src/main/java/com/exam2/exam2/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.exam2.exam2.service;
-
-import com.exam2.exam2.model.service.UserServiceModel;
-
-public interface UserService {
-        void register(UserServiceModel userServiceModel);
-
-}
